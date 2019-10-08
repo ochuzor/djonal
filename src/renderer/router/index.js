3043d41413473056d7f1921b573f457aa593279a
Chinedu Eze <chinedu.eze@magicad.com>
2019-10-08 15:03:17 +0300

    removed login page, renamed default page

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,25 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import store from '../store'
+// import store from '../store'
 
 Vue.use(Router)
 
-const ifNotAuthenticated = (to, from, next) => {
-    if (!store.getters.isAuthenticated) {
-        next()
-        return
-    }
+// const ifNotAuthenticated = (to, from, next) => {
+//     if (!store.getters.isAuthenticated) {
+//         next()
+//         return
+//     }
 
-    next('/')
-}
+//     next('/')
+// }
 
-const ifAuthenticated = (to, from, next) => {
-    if (store.getters.isAuthenticated) {
-        next()
-        return
-    }
-    next('/login')
-}
+// const ifAuthenticated = (to, from, next) => {
+//     if (store.getters.isAuthenticated) {
+//         next()
+//         return
+//     }
+//     next('/login')
+// }
 
 export default new Router({
     routes: [
@@ -28,17 +28,22 @@ export default new Router({
     //   name: 'landing-page',
     //   component: require('@/components/LandingPage').default
     // },
+    // {
+    //     path: '/',
+    //     name: 'entry-list',
+    //     component: require('@/components/EntryList').default,
+    //     beforeEnter: ifAuthenticated
+    // },
+    // {
+    //     path: '/login',
+    //     name: 'login-page',
+    //     component: require('@/components/LoginPage').default,
+    //     beforeEnter: ifNotAuthenticated
+    // },
         {
             path: '/',
-            name: 'entry-list',
-            component: require('@/components/EntryList').default,
-            beforeEnter: ifAuthenticated
-        },
-        {
-            path: '/login',
-            name: 'login-page',
-            component: require('@/components/LoginPage').default,
-            beforeEnter: ifNotAuthenticated
+            name: 'home',
+            component: require('@/components/Home').default
         },
         {
             path: '*',
